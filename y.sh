f424d4e8567a3c27ff10b14e6a07c17442eb161a
rslai <leafmouse00@gmail.com>
2020-05-31 16:21:36 +0800

    write home dir ~

--- y.sh
+++ y.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Program Y is only allowed to read/write files under /var/Y/" > test.txt
+echo "Program Y is only allowed to read/write files under /var/Y/" > ~/test.txt
 telnet ptt.cc
