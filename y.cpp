d14035748d851b997c4832f55626619131073fd4
rslai <leafmouse00@gmail.com>
2020-06-01 10:08:51 +0800

    rw

--- y.cpp
+++ y.cpp
@@ -8,13 +8,42 @@
 
 using namespace std;
 
-int main(){
-	
-	string text = "read write allow?\n";
+void _write(string path){
+    string text = "rw test!\n";
 	ofstream ofs;
-	ofs.open("test.txt");
-	ofs << text;
-	ofs.close();
+	ofs.open(path);
+	if(!ofs){	
+		cout << "[permission denied] fail to write " << path << endl;
+	}else{
+		ofs << text;
+		ofs.close();
+	}
+}
+
+void _read(string path){
+	ifstream ifs;
+	ifs.open(path);
+	if(!ifs){
+		cout << "[permission denied] fail to read " << path << endl;
+	}else{
+		string str;
+		while(getline(ifs, str)){
+			cout << str << endl;
+		}
+	}
+}
+
+int main(){
+
+	string allowed_wpath = "./w.txt", test_wpath = "~/w.txt";
+	string allowed_rpath = "./w.txt", test_rpath = "~/w.txt";
+
+	_write(allowed_wpath);
+	_write(test_wpath);
+
+	_read(allowed_rpath);
+	_read(test_rpath);
+
 
 	struct sockaddr_in server; 
 	int sock;  
@@ -23,10 +52,12 @@ int main(){
 		server.sin_family = AF_INET;  
 		server.sin_port = htons(23);
 		inet_pton(AF_INET, "140.112.172.1", &server.sin_addr.s_addr);
-		if(connect(sock, (struct sockaddr *)&server, sizeof(server)) == 0){
-			cout << "connect to ptt!" << endl;
+		if(connect(sock, (struct sockaddr *)&server, sizeof(server)) != 0){
+			cout << "[permission denied] fail to create connection" << endl;
 		}
 		close(sock);
+	}else{	
+		cout << "[permission denied] fail to create socket" << endl;
 	}
 
 	return 0;
